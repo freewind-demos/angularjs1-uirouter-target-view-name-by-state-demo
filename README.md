757d8e77a93c7834dcbe966c8d350612a15af5b4
freewind <nowindlee@gmail.com>
2019-02-26 17:46:28 +0800

    demo

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-AngularJS1 "@uirouter" Parent Child State Demo
-==============================================
+AngularJS1 "@uirouter" Target View Name by State Demo
+=====================================================
 
-当一个state的名字形如`a.b`的时候，它是state `a`的子state.
+默认情况下，child state中`views`中给出的view name是parent state产生的。
 
-当渲染`a.b`这个子state时，它会先渲染`a`这个父state，然后在`a`的template中包括的`<ui-view>`中，
-再继续渲染`a.b`对应的template。
-
-如果state `a`中的template中没有`<ui-view>`，那么`a.b`对应的template将无处渲染。
+有时候，我们需要打破这个约定，让child state自己选择target到一个不是由parent state产生的uiview中。
 
 ```
 npm install
--- app.js
+++ app.js
@@ -2,22 +2,48 @@ const app = angular.module('app', ['ui.router'])
 
 app.config(($stateProvider, $urlRouterProvider) => {
 
-  $stateProvider.state('parent', {
+  $stateProvider.state('grandparent', {
+    url: '/grandparent',
+    templateUrl: './templates/grandparent.html'
+  });
+
+  $stateProvider.state('grandparent.parent', {
     url: '/parent',
-    templateUrl: './templates/parent.html'
+    views: {
+      head: {
+        templateUrl: './templates/parent-head.html'
+      },
+      body: {
+        templateUrl: './templates/parent-body.html'
+      },
+    }
   });
 
-  $stateProvider.state('parent.child', {
+  $stateProvider.state('grandparent.parent.child', {
     url: '/child',
-    templateUrl: './templates/child.html'
+    views: {
+      head: {
+        templateUrl: './templates/child-head.html'
+      },
+      body: {
+        templateUrl: './templates/child-body.html'
+      },
+    }
   });
 
-  $stateProvider.state('child', {
-    url: '/child',
-    templateUrl: './templates/child.html'
+  $stateProvider.state('grandparent.parent.naughtyChild', {
+    url: '/naughty-child',
+    views: {
+      'head@grandparent': {
+        templateUrl: './templates/child-head.html'
+      },
+      body: {
+        templateUrl: './templates/child-body.html'
+      },
+    }
   });
 
-  $urlRouterProvider.otherwise('/parent');
+  $urlRouterProvider.otherwise('/grandparent');
 
 });
 
